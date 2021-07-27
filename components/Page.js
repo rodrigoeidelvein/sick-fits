d6721c4b3078abdbb15428bfcbd99658757106c4
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2021-07-26 23:42:39 -0300

    first commit

---
+++ components/Page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+export default function Page({ children }) {
+    return (
+        <div>
+            <Header />
+            <h2>I am the page component</h2>
+            {children}
+        </div>
+    );
+}
---
+++ pages/sell.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SellsPage() {
+    return (
+        <div>
+            Sells
+        </div>
+    )
+}
