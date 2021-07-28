92c7ee5ada4f7a7108c35d0f42d17942cad579dd
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2021-07-27 21:18:15 -0300

    fix css loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import "../styles/globals.css";
+import nProgress from "nprogress";
 import Page from "../components/Page";
+import "../components/styles/nprogress.css";
+import { Router } from "next/router";
+
+Router.events.on("routeChangeStart", () => {
+    nProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => {
+    nProgress.done();
+});
+
+Router.events.on("routeChangeError", () => {
+    nProgress.done();
+});
 
 function MyApp({ Component, pageProps }) {
     return (
